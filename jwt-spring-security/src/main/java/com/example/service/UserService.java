0dff9623dd7c8ad754096968bbfe9fab19030723
kolyaratishin <kolyaratishin1@gmail.com>
2023-06-27 16:08:11 +0300

    added user and jwt services

---
+++ jwt-spring-security/src/main/java/com/example/service/UserService.java
@@ -0,0 +1,29 @@
+package com.example.service;
+
+import com.example.model.User;
+import com.example.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    public String addUser(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return "User added to the system";
+    }
+
+    public String generateToken(String username){
+        return jwtService.generateToken(username);
+    }
+
+    public void validateToken(String token){
+        jwtService.validateToken(token);
+    }
+}
