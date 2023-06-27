b98eb4f35bd33ff7c72dfccf21d6c1f03e3e4838
kolyaratishin <kolyaratishin1@gmail.com>
2023-06-27 16:13:48 +0300

    added auth controller

---
+++ jwt-spring-security/src/main/java/com/example/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.example.controller;
+
+import com.example.model.User;
+import com.example.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/jwt")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserService userService;
+
+    @PostMapping("/registration")
+    public String addNewUser(@RequestBody User user){
+        return userService.addUser(user);
+    }
+
+    @GetMapping("/token")
+    public String generateToken(@RequestBody User user){
+        return userService.generateToken(user.getUsername());
+    }
+
+    @GetMapping("/validation")
+    public String validateToken(@RequestParam("token") String token){
+        userService.validateToken(token);
+        return "Token is valid";
+    }
+}
