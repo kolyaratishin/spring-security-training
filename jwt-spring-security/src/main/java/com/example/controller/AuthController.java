adcbdad27e3a23ed1fbe07b4ed38d064f88c25c7
kolyaratishin <kolyaratishin1@gmail.com>
2023-06-27 17:07:43 +0300

    added authentication and UserDetails implementation

--- jwt-spring-security/src/main/java/com/example/controller/AuthController.java
+++ jwt-spring-security/src/main/java/com/example/controller/AuthController.java
@@ -3,6 +3,9 @@ package com.example.controller;
 import com.example.model.User;
 import com.example.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,19 +13,24 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AuthController {
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/registration")
-    public String addNewUser(@RequestBody User user){
+    public String addNewUser(@RequestBody User user) {
         return userService.addUser(user);
     }
 
     @PostMapping("/token")
-    public String generateToken(@RequestBody User user){
-        return userService.generateToken(user.getUsername());
+    public String generateToken(@RequestBody User user) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        if (authentication.isAuthenticated()) {
+            return userService.generateToken(user.getUsername());
+        }
+        throw new RuntimeException("User invalid access");
     }
 
     @GetMapping("/validation")
-    public String validateToken(@RequestParam("token") String token){
+    public String validateToken(@RequestParam("token") String token) {
         userService.validateToken(token);
         return "Token is valid";
     }
