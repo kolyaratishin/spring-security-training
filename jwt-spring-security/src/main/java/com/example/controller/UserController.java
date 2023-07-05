f665f5f4c80535ae828f4d534da6db6ead1bba10
kolyaratishin <kolyaratishin1@gmail.com>
2023-07-05 15:48:54 +0300

    added get all users enpdoint

---
+++ jwt-spring-security/src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.example.controller;
+
+import com.example.model.User;
+import com.example.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
